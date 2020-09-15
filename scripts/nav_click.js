779037db4ad05b537add373503780be561edcb73
vajira <vlbthambawita@gmail. com>
2020-09-15 23:34:08 +0200

    complete side bar

---
+++ scripts/nav_click.js
@@ -0,0 +1,11 @@
+function myAccFunc() {
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-theme-d5";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className = 
+      x.previousElementSibling.className.replace(" w3-theme-d5", "");
+    }
+  }
